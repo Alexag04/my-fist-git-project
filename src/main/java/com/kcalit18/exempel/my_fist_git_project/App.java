3c76a0bcf9629cf8b85f0591b08e3ea65a233515
alexag04 <alexagrell2@hotmail.com>
2018-12-12 11:52:51 +0100

    first

---
+++ src/main/java/com/kcalit18/exempel/my_fist_git_project/App.java
@@ -0,0 +1,13 @@
+package com.kcalit18.exempel.my_fist_git_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
